4dec100dd490356ad5e0bca41ee6c2717cdf268c
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-08-12 12:36:17 +0200

    first commit

---
+++ src/Stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { useGlobalContext } from './context';
+
+const Stories = () => {
+  const { isLoading, hits } = useGlobalContext();
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        const { objectID, title, num_comments, url, points, author } = story;
+        return (
+          <article key={objectID} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} | </span>
+              {num_comments} comments
+            </p>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Stories;
